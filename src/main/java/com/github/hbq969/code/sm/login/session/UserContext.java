f90a1b14e1eadab7d7800f7e07a1e733bbd79ec6
hbq969 <hbq969@gmail.com>
2025-02-17 12:24:21 +0800

    UserContext增加权限校验方法

--- src/main/java/com/github/hbq969/code/sm/login/session/UserContext.java
+++ src/main/java/com/github/hbq969/code/sm/login/session/UserContext.java
@@ -2,6 +2,7 @@ package com.github.hbq969.code.sm.login.session;
 
 import cn.hutool.core.lang.Assert;
 import com.github.hbq969.code.sm.login.model.UserInfo;
+import org.apache.commons.lang3.StringUtils;
 
 public class UserContext {
     private static ThreadLocal<UserInfo> tl = new ThreadLocal<>();
@@ -18,5 +19,13 @@ public class UserContext {
 
     public static void remove() {
         tl.remove();
+    }
+
+    public static boolean sameUser(String userName) {
+        return StringUtils.equals(get().getUserName(), userName);
+    }
+
+    public static boolean permitAllow(String userName) {
+        return get().isAdmin() || sameUser(userName);
     }
 }
